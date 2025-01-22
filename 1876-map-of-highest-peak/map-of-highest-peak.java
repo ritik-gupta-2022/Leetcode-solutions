322f36aafdb30046eeac6d524e37dcd2c13bd47e
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2025-01-22 23:49:48 +0530

    Time: 47 ms (78.86%) | Memory: 166.6 MB (72.76%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] highestPeak(int[][] arr) {
+        int m = arr.length, n = arr[0].length;
+        Queue<int[]> q= new LinkedList<>();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i][j] == 1){
+                    arr[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                }
+                else arr[i][j]=-1;
+            }
+        }
+        int d[][] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while(!q.isEmpty()){
+            int temp[] = q.poll();
+            int x = temp[0] , y= temp[1];
+            for(int a[]:d){
+                int nx = x + a[0], ny = y + a[1];
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n && arr[nx][ny] == -1){
+                    arr[nx][ny] = arr[x][y]+1;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+        
+        return arr;
+    
+    }
+}
