3fd492bb269e586d4241c1c2985057f340d96ea8
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2026-02-26 02:26:47 +0530

    Time: 3 ms (53.73%) | Memory: 49.4 MB (8.39%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,22 +1,32 @@
 class Solution {
     List<String> ls = new ArrayList<>();
     Set<String> set = new HashSet<>();
+    Map<String,List<String>> dp = new HashMap<>();
 
-    public void helper(String s, String ans){
-        if(s.length()==0){
-            ls.add(ans.trim());
-            return;
+    public List<String> helper(String s){
+        List<String> result = new ArrayList<>();
+
+        if(s.length() == 0){
+            result.add("");
+            return result;
         }
+
+        if(dp.containsKey(s))  return dp.get(s);
+
         for(int i=1; i<=s.length(); i++){
-            if(set.contains(s.substring(0,i))){
-                helper(s.substring(i), ans+s.substring(0,i)+" ");
+            String curr = s.substring(0,i);
+            if(set.contains(curr)){
+               List<String> rem =  helper(s.substring(i));
+                for(String str: rem){
+                    result.add((curr +" "+str).trim());
+                }
             }
         }
-
+        dp.put(s,result);
+        return result;
     }
     public List<String> wordBreak(String s, List<String> wordDict) {
         for(String str:wordDict) set.add(str);
-        helper(s,"");
-        return ls;
+        return helper(s);
     }
 }
