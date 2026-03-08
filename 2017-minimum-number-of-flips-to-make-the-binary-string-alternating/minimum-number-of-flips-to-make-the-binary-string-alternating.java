88d1f1b166ae2ffb2872a1536325e0db5a9e9bf5
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2026-03-09 01:48:26 +0530

    Time: 19 ms (75.74%) | Memory: 47.6 MB (25.55%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,18 +1,45 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        int res = n;
-        int[] op = {0, 0};
+        int stzero = 0, stone = 0;
 
-        for (int i = 0; i < n; i++)
-            op[(s.charAt(i) ^ i) & 1]++;
-
-        for (int i = 0; i < n; i++) {
-            op[(s.charAt(i) ^ i) & 1]--;
-            op[(s.charAt(i) ^ (n + i)) & 1]++;
-            res = Math.min(res, Math.min(op[0], op[1]));
+        for(int i=0; i<n; i++){
+            if((i&1)==0){
+                if(s.charAt(i)=='0') stone++;
+                else stzero++;
+            }
+            else{
+                if(s.charAt(i)=='0') stzero++;
+                else stone++;
+            }
         }
+        int ans = Math.min(stzero,stone);
+        int l=0, r=n;
+        s = s+s;
 
-        return res;
+        for(int i=1; i<n; i++){
+            if((l&1)==0){
+                if(s.charAt(l)=='0') stone--;
+                else stzero--;
+            }
+            else{
+                if(s.charAt(l)=='0') stzero--;
+                else stone--;
+            }
+
+            if((r&1)==0){
+                if(s.charAt(r)=='0') stone++;
+                else stzero++;
+            }
+            else{
+                if(s.charAt(r)=='0') stzero++;
+                else stone++;
+            }
+
+            ans = Math.min(ans, Math.min(stone, stzero));
+
+            l++; r++;
+        }
+        return ans;
     }
 }
