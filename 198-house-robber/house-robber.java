b999dc052de9c8fc149396e6cf9c60b23a9a76c0
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2026-03-15 22:26:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (31.88%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -16,8 +16,6 @@ class Solution {
         int prev = nums[0];
         int prev2 = 0;
         int curr=0;
-        int neg=0;
-
         
         for(int i=1; i<nums.length; i++){
             int rob = nums[i] + prev2;
