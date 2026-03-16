c46765dd778768574816018ef8ff243c068e1edd
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2026-03-16 23:43:20 +0530

    Time: 1 ms (17.49%) | Memory: 43.5 MB (60.38%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -14,9 +14,22 @@ class Solution {
     }
     public int uniquePathsWithObstacles(int[][] arr) {
         int m = arr.length, n = arr[0].length;
-        if(arr[m-1][n-1]==1) return 0;
         dp = new int[m][n];
-        for(int i[]: dp) Arrays.fill(i,-1);
-        return helper(arr,m-1,n-1);
+        if(arr[m-1][n-1]==1) return 0;
+
+        dp[0][0] = 1;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) continue;
+
+                int a=0, b=0;
+                if(i>0 && arr[i-1][j]!=1) a =  dp[i-1][j];
+                if(j>0 && arr[i][j-1]!=1) b = dp[i][j-1];
+
+                dp[i][j]=a+b;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
