63fde648706f0ffffd6e8118c1640cfd1a09407b
Ritik Gupta <132193172+ritik-gupta-2022@users.noreply.github.com>
2026-03-16 23:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (61.31%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -14,8 +14,21 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         dp = new int[m][n];
-        for(int i[]: dp) Arrays.fill(i,-1);
 
-        return helper(m-1,n-1);
+        dp[0][0] = 1;
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) continue;
+
+                int a = 0, b =0;
+                if(i>0) a = dp[i-1][j];
+                if(j>0) b = dp[i][j-1];
+
+                dp[i][j] = a+b;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 }
